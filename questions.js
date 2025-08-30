d06ff503656d52da6e6210bc6c7b046525e17256
GoldaPrincy <132034577+GoldaPrincy@users.noreply.github.com>
2025-08-30 08:30:05 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer:"C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question:" Which attribute is used to specify the source (URL) of an image in HTML?",
+        answer:"B. src",
+        options: [
+            "A. alt",
+            "B. src",
+            "C. link",
+            "D. href"
+        ]
+    },
+    {
+        numb: 3,
+        question:" Which attribute is used to provide an alternative text for an image if it cannot be displayed?",
+        answer:"C. alt",
+        options: [
+            "A. src",
+            "B. title",
+            "C. alt",
+            "D. description"
+        ]
+    },
+    {
+        numb: 4,
+        question:"What does CSS stand for?",
+        answer:"C. Cascading Style Sheet",
+        options: [
+            "A. Creative Style Sheet", 
+            "B. Computer Style Sheet", 
+            "C. Cascading Style Sheet",
+            "D. Colorful Style Sheets"
+        ]
+    },
+    {
+        numb: 5,
+        question:"What is the purpose of the display: flex; property?",
+        answer:"B. To create a flexible container for arranging items",
+        options: [
+            "A. To make an element invisible",
+            "B. To create a flexible container for arranging items",
+            "C. To add a shadow effect to an element",
+            "D. To change the font size of text"
+
+        ]
+    },
+    {
+        numb: 6,
+        question:"Which CSS property controls the spacing between the content and the border of an element?",
+        answer:"B. padding",
+        options: [
+            "A. margin", 
+            "B. padding", 
+            "C. border-spacing", 
+            "D. spacing"
+        ]
+    },
+    {
+        numb: 7,
+        question:"Which CSS property is used to change the text color of an element? ",
+        answer:"C. color",
+        options: [
+            "A. font-color", 
+            "B. text-color", 
+            "C. color", 
+            "D. foreground-color"
+        ]
+    },
+    {
+        numb: 8,
+        question:"What is the purpose of the typeof operator in JavaScript?",
+        answer:"B. To determine the data type of a variable",
+        options: [
+           "A. To check if a variable is defined",
+           "B. To determine the data type of a variable",
+           "C. To convert a variable to a different data type",
+           "D. To check if a variable is an array"         
+
+        ]
+    },
+    {
+        numb: 9,
+        question:"Which method is used to add an element to the end of an array in JavaScript?",
+        answer:"C. push()",
+        options: [
+            "A. pop()",
+            "B. shift()",
+            "C. push()",
+            "D. splice()"
+        ]
+    },
+    {
+        numb: 10,
+        question:"What is the result of the expression '5' + 2 in JavaScript?",
+        answer:"B. '52'",
+        options: [
+            "A. 7",
+            "B. '52'",
+            "C. NaN",
+            "D. undefined"
+        ]
+    }
+
+];
